63e10634aaa5405c4c399b1a84662b4b8af3289c
aryanknight <aryanknight12@gmail.com>
2023-07-30 13:49:28 +0530

    testing-1

--- lib/actions.ts
+++ lib/actions.ts
@@ -8,7 +8,6 @@ const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : '
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
 const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
 
-
 const client = new GraphQLClient(apiUrl);
 
 export const fetchToken = async () => {
