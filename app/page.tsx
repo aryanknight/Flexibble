680d035f739e46097f22e7bda9d6e87a581e5dab
aryanknight <aryanknight12@gmail.com>
2023-07-30 12:43:16 +0530

    completed

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
 type SearchParams = {
-  category?: string ;
+  category?: string | null;
   endcursor?: string | null;
 }
 
